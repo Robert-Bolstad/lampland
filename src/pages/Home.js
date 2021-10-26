223ba4030cb9af36e57f2ab767658f6585ecf625
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-10-27 01:32:50 +0200

    Feat: added hero banner to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
-import React from "react";
+import HeroBanner from "../components/HeroBanner";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <HeroBanner />
+    </>
+  );
 };
 
 export default Home;
