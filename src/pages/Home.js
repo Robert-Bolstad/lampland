005882f12cd457b0e229550a9843c10870d9182a
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-10-27 23:29:05 +0200

    Feat: added featured component to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import HeroBanner from "../components/HeroBanner";
 import Usp from "../components/Usp";
+import Featured from "../components/Featured";
 
 const Home = () => {
   return (
     <>
       <HeroBanner />
       <Usp />
+      <Featured />
     </>
   );
 };
