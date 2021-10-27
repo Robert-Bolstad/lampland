b84e00f12a126d3bf9c74abc73337865f43c3fba
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-10-27 04:35:06 +0200

    Feat: added Usp component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import HeroBanner from "../components/HeroBanner";
+import Usp from "../components/Usp";
 
 const Home = () => {
   return (
     <>
       <HeroBanner />
+      <Usp />
     </>
   );
 };
