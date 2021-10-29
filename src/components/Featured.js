d148b629b2008de0977eaa15eaca033e6c417eef
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-10-29 02:17:18 +0200

    Update: removed code for cards

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -2,6 +2,7 @@ import { apiUrl } from "../utils/api";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { BsLampFill } from "react-icons/bs";
+import Card from "./Card";
 
 const Featured = () => {
   const [featured, SetFeatured] = useState([]);
@@ -25,26 +26,7 @@ const Featured = () => {
   return (
     <section className="featured">
       <h1 className="featured__heading">Featured Products</h1>
-      <div className="featured__container">
-        {featured.map((product) => {
-          return (
-            <div className="featured__card" key={product.id}>
-              <img
-                className="featured__img"
-                src={product.image_url}
-                alt="Image of a lamp"
-              />
-              <Link to="/">
-                <h3 className="featured__productName">{product.name}</h3>
-              </Link>
-
-              <p className="featured__description">
-                {product.description.substring(0, 100)}
-              </p>
-            </div>
-          );
-        })}
-      </div>
+      <Card products={featured} />
       <Link className="featured__link" to="/products">
         <BsLampFill className="featured__icon" /> View All Lamps
       </Link>
