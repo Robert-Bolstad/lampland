57ae22ab789ecf1f48ae0f74fe258bda32857311
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-10-30 14:03:28 +0200

    update: added increment function

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,8 +1,7 @@
 import { apiUrl } from "../utils/api";
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
-import { AiFillPlusCircle } from "react-icons/ai";
-import { AiFillMinusCircle } from "react-icons/ai";
+import IncrementDecrement from "../components/IncrementDecrement";
 import { FaShoppingCart } from "react-icons/fa";
 import { saveToStorage, retrieveFromStorage } from "../utils/localStorage";
 import Modal from "../components/Modal";
@@ -20,35 +19,14 @@ const ProductDetails = () => {
     setProduct(product);
   };
 
-  const incrementDecrement = (value) => {
-    let quantityNr = quantity;
-
-    if (value === 1) {
-      quantityNr = quantity + 1;
-    } else {
-      quantityNr = quantity - 1;
-    }
-
-    switch (quantityNr) {
-      case 0:
-        setQuantity(1);
-        break;
-      case 100:
-        setQuantity(99);
-        break;
-      default:
-        setQuantity(quantityNr);
-        break;
-    }
-  };
-
   const addToCart = () => {
     setModalState(true);
 
     let products = retrieveFromStorage("cart");
 
     const data = {
-      id: Math.random(),
+      id: id,
+      cartId: Math.random(),
       name: product.name,
       price: product.price,
       quantity: quantity,
@@ -89,22 +67,7 @@ const ProductDetails = () => {
         <p className="detail__description">{product.description}</p>
         <div className="detail__wrapper">
           <span className="detail__price">{product.price + " $"}</span>
-          <div className="detail__quantity">
-            <input className="detail__input" type="number" />
-            <button
-              onClick={() => incrementDecrement(-1)}
-              className="detail__decrement"
-            >
-              <AiFillMinusCircle className="detail__icon" />
-            </button>
-            <div className="detail__number">{quantity}</div>
-            <button
-              onClick={() => incrementDecrement(1)}
-              className="detail__increment"
-            >
-              <AiFillPlusCircle className="detail__icon" />
-            </button>
-          </div>
+          <IncrementDecrement quantity={quantity} setQuantity={setQuantity} />
         </div>
         <div className="detail__cart">
           <button onClick={() => addToCart()} className="detail__cart--btn">
