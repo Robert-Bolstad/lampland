79f567ccd012dbe80ec8eeb6bd791229bef8f8da
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-10-30 10:36:11 +0200

    feat: added links

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,14 +6,20 @@ const Card = ({ products }) => {
       {products.map((product) => {
         return (
           <div className="card__container" key={product.id}>
-            <img
-              className="card__img"
-              src={product.image_url}
-              alt="Image of a lamp"
-            />
-            <Link to="/">
-              <h3 className="card__headline">{product.name}</h3>
+            <Link to={`/details/${product.id}`}>
+              <img
+                className="card__img"
+                src={product.image_url}
+                alt="Image of a lamp"
+              />
             </Link>
+            <div className="card__headingWrapper">
+              <Link to={`/details/${product.id}`}>
+                <h3 className="card__headline">{product.name}</h3>
+              </Link>
+
+              <span className="card__price">{product.price + "$"}</span>
+            </div>
 
             <p className="card__description">
               {product.description.substring(0, 100)}
