3e0abf6e1430e8737ce7955a17f2e257d0b5a62f
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-10-31 17:25:44 +0100

    feat: added code to login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,18 @@
 import React from "react";
+import LoginForm from "../components/LoginForm";
+import { RiLoginBoxFill } from "react-icons/ri";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <div className="login__wrapper">
+        <RiLoginBoxFill className="login__icon" />
+        <h1 className="login__heading">Login</h1>
+      </div>
+
+      <LoginForm />
+    </div>
+  );
 };
 
 export default Login;
