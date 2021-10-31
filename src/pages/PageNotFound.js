538b462c70f3a13f333cc7f77c30983e0b04a3c3
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-10-31 20:08:52 +0100

    feat: added code

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,7 +1,13 @@
-import React from "react";
+import logo from "../img/logo.svg";
 
 const PageNotFound = () => {
-  return <div>This page does not exist</div>;
+  return (
+    <div className="notFound">
+      <img className="notFound__logo" src={logo} alt="" />
+
+      <h1 className="notFound__heading">Page dose not exsist!</h1>
+    </div>
+  );
 };
 
 export default PageNotFound;
